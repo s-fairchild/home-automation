800c6ff3e6c979aa17ff35773584765cb272b81d
s-fairchild <steve51516@gmail.com>
2023-01-12 19:51:56 -0500

    Update motionplus init
    Use podman run --init and --init-path to initialize configuration files for motionplus.
    This removes the need to download and run a bash image as a pre task.

--- fedora_coreos/files/bin/motion_config_editor.sh
+++ fedora_coreos/files/bin/motion_config_editor.sh
@@ -1,14 +1,21 @@
-#!/bin/bash -x
+#!/bin/bash
+#
+# Init script for motionplus container
+# Replaces placeholder text in configuration files with podman secrets
+
+set -o nounset \
+    -o errexit
 
 main() {
-    tmp_dir="/mnt/temp_config"
-    if [ -n "$1" ] && [ -d "$1" ]; then
-        tmp_dir="$1"
-    elif [ ! -d "$1" ] && [ -n "$1" ]; then
-        echo "$1 must be an existing directory"
-        exit 1
+    tmp_dir="/mnt/motionplus"
+    if [ ! -d "$tmp_dir" ]; then
+        abort "${tmp_dir} not found"
     fi
-    cp /mnt/motion/*.conf "$tmp_dir"
+
+    dest_conf="/usr/local/etc/motionplus"
+    pushd "$tmp_dir"
+    cp ./*.conf "${dest_conf}/" || abort "failed to copy ${tmp_dir}/*.conf to ${dest_conf}/"
+    popd
 
     secrets=(
         "$motion_camera1"
@@ -24,9 +31,8 @@ main() {
 
     i=1
     for s in ${secrets[@]}; do
-        if ! sed -i "s/PLACEHOLDER/$s/" "${tmp_dir}/camera${i}.conf"; then
-            echo "Failed to replace placeholder"
-            exit 1
+        if ! sed -i "s/PLACEHOLDER/${s}/" "${dest_conf}/camera${i}.conf"; then
+            abort "Failed to replace placeholder"
         fi
         ((i++))
     done
@@ -34,20 +40,24 @@ main() {
 
     i=1
     for s in ${url_secrets[@]}; do
-        if ! sed -i "s/URI/$s/" "${tmp_dir}/camera${i}.conf"; then
-            echo "Failed to replace URI placeholder"
-            exit 1
+        if ! sed -i "s/URI/${s}/" "${dest_conf}/camera${i}.conf"; then
+            abort "Failed to replace URI placeholder"
         fi
         ((i++))
     done
+
+    unset secrets url_secrets
+
+    if [[ $1 == "--" ]]; then
+        shift
+        echo "Executing: $*"
+	    "$@"
+    fi
 }
 
-#######################################
-# abort echos a message and exits with code 1
-#######################################
 abort() {
     echo "${1}, Aborting."
     exit 1
 }
 
-main "$1"
+main "$@"
