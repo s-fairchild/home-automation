8bf7e8fb93d6713ee2b2c84b9de6e84b52c3a20e
s-fairchild <steve51516@gmail.com>
2023-02-04 14:51:00 -0500

    WIP Seperate k3d cluster install script into local file
    It's getting too big

---
+++ fedora_coreos/files/k3d/k3d-create-cluster.sh
@@ -0,0 +1,83 @@
+#!/bin/bash -x
+#
+# Run post k3d install
+
+main() {
+    CLUSTER="${CLUSTER:-titan}"
+    K3D_REGISTRY="${K3D_REGISTRY:-k3d-registry}"
+    K3D_NETWORK="${K3D_NETWORK:-k3d}"
+    K3D_CLUSTER_YAML="${K3D_CLUSTER_YAML:-/usr/local/etc/k3d/k3d-cluster.yaml}"
+    
+    # Run in a subshell to keep script shell from exiting
+    if [[ ! -f /usr/local/bin/k3d ]]; then
+        bash -c "/usr/local/bin/k3d-installer.sh || echo "failed to install k3d! Exiting" && exit 1"
+    fi
+
+    # A new bridge network must be created because the default podman network doesn't
+    # provided dns
+    if ! podman network exists "$K3D_NETWORK"; then
+        podman network create "$K3D_NETWORK"
+    fi
+
+    # Using FCOS Butane links doesn't work for some reason
+    # setup_podman_socket
+
+    # k3d looks for docker's default network named "bridge"
+    # We must create the registry using our new network with dns
+    # to host our registry
+    if ! podman container exists "$K3D_REGISTRY"; then
+        k3d registry create --verbose --default-network k3d -p 5000
+    fi
+
+    # Use registry currently doesn't work as a yaml config option
+    if ! k3d cluster list | grep titan &> /dev/null; then
+        if k3d cluster create --verbose --registry-use "$K3D_REGISTRY" -c "${K3D_CLUSTER_YAML}"; then
+            label_nodes
+            create_kubeconfig
+            create_bashcompletion
+        fi
+    else
+        echo -e "Aborting... ${CLUSTER} found.\nIs there another ${CLUSTER} present?"
+    fi
+          
+}
+
+# setup_podman_socket setups podman to cover docker's socket connection for k3d
+# Note: this WILL NOT WORK Environment=DOCKER_HOST=unix:/run/podman/podman.sock
+# k3d tries and fails to connect to that endpoint, but /var/run/docker.sock works
+setup_podman_socket() {
+    systemctl disable --now docker docker.socket 
+    systemctl mask docker docker.socket
+              
+    rm -f /var/run/docker.sock
+    systemctl enable --now podman.socket
+    systemctl restart podman.socket
+    ln -s /run/podman/podman.sock /var/run/docker.sock
+}
+
+# shellcheck disable=SC2120
+label_nodes() {
+    node_prefix="${1:-k3d-$cluster-agent}"
+    for i in ${1..3}; do
+        kubectl label "node/${node_prefix}-${i}" node-role.kubernetes.io/worker=true 
+    done
+}
+
+create_bashcompletion() {
+    local bash_k3d=/etc/bash_completion.d/k3d
+    if [ ! -f $bash_k3d ]; then
+        k3d completion bash > "${bash_k3d}"
+    fi
+}
+
+create_kubeconfig() {
+    kubeconfig="$HOME/.kube"
+    if [[ ! -d "$kubeconfig" ]]; then
+        mkdir "$kubeconfig"
+    fi
+    k3d kubeconfig get titan > "$kubeconfig"
+
+    echo "kubeconfig located at ${kubeconfig}"
+}
+
+main "$@"
